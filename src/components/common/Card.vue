c825d08bab9b329bbc273c5c52aa479cb42a64ad
tojoniaina <tojorar@gmail.com>
2023-03-15 14:44:50 +0300

    first commit efa vita ny creation dossier sy ny element anaty card

---
+++ src/components/common/Card.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name:"name"
+}
+</script>
+
+<template >
+    <div class="main">
+        <div class="card">
+            <div class="card__header">
+                <img src="" alt="">
+                <h2>{{ firstName }}  {{ lastName }}</h2>
+                <h3> {{ email }}</h3>
+            </div>
+            <div class="card__exp">
+                <h2><img src="../../assets/Untitled (22)/roles.png" alt="">{{ role }}</h2>
+                <h2><img src="../../assets/Untitled (22)/age.png" alt="">{{ age }}</h2>
+                <h2><img src="../../assets/Untitled (22)/skills.png" alt="">{{ skills }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style >
+    
+</style>
